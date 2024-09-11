46c08296beb85d5a6363d3357d6a3bbef130ecda
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-09-11 14:39:01 +0530

    sidewiseTriangle

--- Patterns/star patterns/downPyramid.cpp
+++ Patterns/star patterns/downPyramid.cpp
@@ -7,20 +7,16 @@
 using namespace std;
 
 void pattern(int n){
-	for (int i = 0; i < n; i++)
+	for (int i = n; i >0; i--)
 
 	{
-		for (int j = 0; j <i; j++)
+		for (int j = 1; j <n-i; j++)
 		{
 			cout<<" ";
 		}
-		for (int j = 0; j < 2*n - (2*i + 1); j++)
+		for (int j = 1; j <= (2*i - 1); j++)
 		{
 			cout<<"*";
-		}
-		for (int j = 0; j<i; j++)
-		{
-			cout<<" ";
 		}
 
 		cout<<endl;
