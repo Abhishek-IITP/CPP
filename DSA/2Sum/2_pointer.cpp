e6a684a33ce9815a291d48bd509aa0ad05de0352
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-10-08 11:07:12 +0530

    Update 2_pointer.cpp

--- DSA/2Sum/2_pointer.cpp
+++ DSA/2Sum/2_pointer.cpp
@@ -5,35 +5,32 @@
 using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target) {
-    vector<pair<int, int>> indexedNums;  // Pair each number with its original index
+    vector<pair<int, int>> indexedNums; 
     
     for (int i = 0; i < nums.size(); i++) {
         indexedNums.push_back({nums[i], i});
     }
-
-    // Sort based on the numbers (not indices)
     sort(indexedNums.begin(), indexedNums.end());
 
-    // Two pointers technique
     int left = 0, right = nums.size() - 1;
 
     while (left < right) {
         int sum = indexedNums[left].first + indexedNums[right].first;
         
         if (sum == target) {
-            return {indexedNums[left].second, indexedNums[right].second};  // Return original indices
+            return {indexedNums[left].second, indexedNums[right].second}; 
         } else if (sum < target) {
-            left++;  // Move the left pointer right
+            left++;  
         } else {
-            right--;  // Move the right pointer left
+            right--;  
         }
     }
 
-    return {};  // No solution found
+    return {};  
 }
 
 int main() {
-    vector<int> nums = {2, 7, 11, 15};  // Example input
+    vector<int> nums = {2, 7, 11, 15};  
     int target = 9;
 
     vector<int> result = twoSum(nums, target);
